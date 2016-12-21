38c75135402477ff3c687213ecc1b521f5a609cc
Mario Gualtieri <mario.gualtieri@3punto6.com>
2016-12-21 16:21:44 +0100

    Change Login UX

--- views/Index.tsx
+++ views/Index.tsx
@@ -1,19 +1,27 @@
 import {View} from "ninjagoat";
 import IndexViewModel from "../scripts/IndexViewModel";
 import * as React from "react";
-import {Button,PageHeader} from "react-bootstrap";
+import {Button,Row} from "react-bootstrap";
 
 export default class Index extends View<IndexViewModel> {
 
     render() {
         return (
             <div>
-                <PageHeader>Command And Control GUI<small>API Projections</small></PageHeader>
-                <input className="form-control" id="apiKey" value={this.viewModel.token} placeholder="Insert API Key" onChange={this.viewModel.setToken}/>
-                <input className="form-control" id="endpoint" value={this.viewModel.endPoint} placeholder="Endpoint" onChange={this.viewModel.setEndPoint}/>
-                <input className="form-control" id="Path" value={this.viewModel.path} placeholder="Path WebSocket" onChange={this.viewModel.setPath}/>
-                <br />
-                <Button onClick={this.viewModel.enterLogin} type="submit" bsStyle="primary" bsSize="large" block>Enter</Button>
+                <Row>
+                    <div className="col-sm-6 col-md-4 col-md-offset-4">
+                        <h1 className="text-center login-title">Command And Control GUI</h1>
+                        <div className="account-wall">
+                                <form className="form-signin">
+                                    <input className="form-control" id="apiKey" value={this.viewModel.token} placeholder="Insert API Key" onChange={this.viewModel.setToken}/>
+                                    <input className="form-control" id="endpoint" value={this.viewModel.endPoint} placeholder="Endpoint" onChange={this.viewModel.setEndPoint}/>
+                                    <input className="form-control" id="Path" value={this.viewModel.path} placeholder="Path WebSocket" onChange={this.viewModel.setPath}/>
+                                    <br />
+                                    <Button onClick={() => this.viewModel.enterLogin()} type="button" bsStyle="primary" bsSize="large" block>Enter</Button>
+                                </form>
+                        </div>
+                    </div>
+                </Row>
             </div>
         );
     }
