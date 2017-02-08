05a1549e4fec1b395bbcd38696e3eec37a7cb3a9
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-02-08 11:24:13 +0100

    Align ProjectionCommand
    Add Restart Command and Delete PauseCommand

--- scripts/command/ProjectionCommand.ts
+++ scripts/command/ProjectionCommand.ts
@@ -1,18 +1,6 @@
 import {CommandDecorators as command} from "ninjagoat-commands";
 import {Transport} from "ninjagoat-commands";
 
-@command.Type("PauseProjection")
-@command.Endpoint("/api/projections/pause")
-@command.Transport(Transport.HTTP_Post)
-export class PauseProjectionCommand {
-    projectionName: string;
-
-    constructor(projectionName: string) {
-        this.projectionName = projectionName;
-    }
-}
-
-
 @command.Type("StopProjection")
 @command.Endpoint("/api/projections/stop")
 @command.Transport(Transport.HTTP_Post)
@@ -25,10 +13,10 @@ export class StopProjectionCommand {
 }
 
 
-@command.Type("ResumeProjection")
-@command.Endpoint("/api/projections/resume")
+@command.Type("RestartProjection")
+@command.Endpoint("/api/projections/restart")
 @command.Transport(Transport.HTTP_Post)
-export class ResumeProjectionCommand {
+export class RestartProjectionCommand {
     projectionName: string;
 
     constructor(projectionName: string) {
