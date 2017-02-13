abc5ece2b68081e6c3974fa662f753935ba435d4
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-02-13 15:17:11 +0100

    Refactor DiagnosticModelRetrieverSpec

--- test/DiagnosticModelRetrieverSpec.ts
+++ test/DiagnosticModelRetrieverSpec.ts
@@ -1,6 +1,7 @@
 import expect = require("expect.js");
+import * as TypeMoq from "typemoq";
 import {DiagnosticModelRetriever} from "../scripts/DiagnosticModelRetriever";
-import {IHttpClient, HttpResponse} from "ninjagoat";
+import {IHttpClient, HttpResponse, IBaseConfig} from "ninjagoat";
 import {IBaseConfigRetriever} from "../scripts/configs/IBaseConfigRetriever";
 import {ITokenRetriever} from "../scripts/configs/ITokenRetriever";
 import {MockHttpClient} from "./fixtures/MockHttpClient";
@@ -9,7 +10,6 @@ import MockTokenRetriever from "./fixtures/MockTokenRetriever";
 import {ISystemProjection} from "../scripts/projection/ISystemProjection";
 import {ModelState} from "ninjagoat-projections";
 import {Observable} from "rx";
-import * as TypeMoq from "typemoq";
 import {IDiagnosticProjection} from "../scripts/projection/IDiagnosticProjection";
 
 describe("Given a DiagnosticModelRetrieverSpec and an observable for System Projection", () => {
@@ -18,6 +18,7 @@ describe("Given a DiagnosticModelRetrieverSpec and an observable for System Proj
         baseConfigRetriever: TypeMoq.Mock<IBaseConfigRetriever>,
         tokenRetriever: TypeMoq.Mock<ITokenRetriever>,
         systemProjection: ISystemProjection,
+        baseConfig: IBaseConfig,
         subject: DiagnosticModelRetriever;
 
     beforeEach(() => {
@@ -30,11 +31,12 @@ describe("Given a DiagnosticModelRetrieverSpec and an observable for System Proj
     context("when receive a notification", () => {
         beforeEach(() => {
             systemProjection = {"events": 10, "projections": {"Crop": {"dependencies": []}}};
+            baseConfig = {"endpoint": "testEndpoint"};
 
-            baseConfigRetriever.setup(b => b.baseConfig()).returns(() => {return {"endpoint": "testEndpoint"}});
+            baseConfigRetriever.setup(b => b.baseConfig()).returns(() => baseConfig);
             tokenRetriever.setup(t => t.token()).returns(() => "testToken");
 
-            httpClient.setup(h => h.get(TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).returns(() => {
+            httpClient.setup(h => h.get("testEndpoint/api/projections/stats/Crop", TypeMoq.It.isAny())).returns(() => {
                 return Observable.just(new HttpResponse({
                     "name": "Crop",
                     "size": 10,
