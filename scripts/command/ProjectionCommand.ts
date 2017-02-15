ee449d2256840b0bb1ed7e7ce8985330e7494c07
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-02-15 17:22:45 +0100

    Revert "Add Event tracker"
    This reverts commit 279e4480ec86675bacd7cac022e8a51dfbf1dad0.

--- scripts/command/ProjectionCommand.ts
+++ scripts/command/ProjectionCommand.ts
@@ -1,11 +1,9 @@
 import {CommandDecorators as command} from "ninjagoat-commands";
 import {Transport} from "ninjagoat-commands";
-import {Event} from "ninjagoat-analytics";
 
 @command.Type("StopProjection")
 @command.Endpoint("/api/projections/stop")
 @command.Transport(Transport.HTTP_Post)
-@Event("projections", "stopProjection", "stop")
 export class StopProjectionCommand {
     projectionName: string;
 
@@ -18,7 +16,6 @@ export class StopProjectionCommand {
 @command.Type("RestartProjection")
 @command.Endpoint("/api/projections/restart")
 @command.Transport(Transport.HTTP_Post)
-@Event("projections", "restartProjection", "restart")
 export class RestartProjectionCommand {
     projectionName: string;
 
@@ -29,7 +26,6 @@ export class RestartProjectionCommand {
 
 @command.Type("StatsProjection")
 @command.Endpoint("/api/projections/stats")
-@Event("projections", "statsProjection", "stats")
 export class StatsProjectionCommand {
     projectionName: string;
 
--- scripts/command/SnapshotCommand.ts
+++ scripts/command/SnapshotCommand.ts
@@ -1,11 +1,9 @@
 import {CommandDecorators as command} from "ninjagoat-commands";
 import {Transport} from "ninjagoat-commands";
-import {Event} from "ninjagoat-analytics";
 
 @command.Type("SaveSnapshot")
 @command.Endpoint("/api/snapshots/save")
 @command.Transport(Transport.HTTP_Post)
-@Event("snapshot", "saveSnapshot", "save")
 export class SaveSnapshotCommand {
     projectionName: string;
 
@@ -14,9 +12,9 @@ export class SaveSnapshotCommand {
     }
 }
 
+
 @command.Type("DeleteSnapshot")
 @command.Endpoint("/api/snapshots/delete")
-@Event("snapshot", "deleteSnapshot", "delete")
 @command.Transport(Transport.HTTP_Post)
 export class DeleteSnapshotCommand {
     projectionName: string;
