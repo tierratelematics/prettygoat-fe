144ab67ba0933552b2d94b9d96dabc44f43f3223
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-02-15 17:34:59 +0100

    login event tracker

--- scripts/command/AuthorizationCommand.ts
+++ scripts/command/AuthorizationCommand.ts
@@ -4,6 +4,7 @@ import {Transport} from "ninjagoat-commands";
 @command.Type("AuthorizationCheck")
 @command.Endpoint("/api/authorization/check")
 @command.Transport(Transport.HTTP_Post)
+@Event("authorization", "loginRequest", "login")
 class AuthorizationCommand {
     token: string;
 
