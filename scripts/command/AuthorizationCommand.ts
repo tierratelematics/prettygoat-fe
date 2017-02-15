0d8feacc4e48c4097d65b5d13d6f694fa84cedd3
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-02-15 17:36:52 +0100

    Fix import for Login Event tracker

--- scripts/command/AuthorizationCommand.ts
+++ scripts/command/AuthorizationCommand.ts
@@ -1,5 +1,6 @@
 import {CommandDecorators as command} from "ninjagoat-commands";
 import {Transport} from "ninjagoat-commands";
+import {Event} from "ninjagoat-analytics";
 
 @command.Type("AuthorizationCheck")
 @command.Endpoint("/api/authorization/check")
