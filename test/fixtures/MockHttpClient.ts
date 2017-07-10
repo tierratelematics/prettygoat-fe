609fa772c7debd09b42dd892bfda398ae267c6b9
Mario Gualtieri <mario.gualtieri2012@gmail.com>
2017-07-10 11:37:19 +0200

    Remove include RX d.ts

--- test/fixtures/MockHttpClient.ts
+++ test/fixtures/MockHttpClient.ts
@@ -4,20 +4,20 @@ import {Dictionary, IHttpClient, HttpResponse} from "ninjagoat"
 export class MockHttpClient implements IHttpClient {
     public getMethodExecuted: boolean = false;
 
-    get(url: string, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
+    get(url: string, headers?: Dictionary<string>): Observable<HttpResponse> {
         this.getMethodExecuted = true;
         return Observable.fromPromise<HttpResponse>(Promise.resolve({}))
     }
 
-    post(url: string, body: any, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
+    post(url: string, body: any, headers?: Dictionary<string>): Observable<HttpResponse> {
         return null
     }
 
-    put(url: string, body: any, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
+    put(url: string, body: any, headers?: Dictionary<string>): Observable<HttpResponse> {
         return null
     }
 
-    delete(url: string, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
+    delete(url: string, headers?: Dictionary<string>): Observable<HttpResponse> {
         return null
     }
 }
